1024af2b6b5ccabeb6c88843485a3c4828efa617
Rostislav Provodenko <rostislav.provodenko@gmail.com>
2023-10-17 22:36:08 +0300

    wip

--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -1,8 +1,19 @@
-
-pub fn bubble_sort(vector: Vec<i32>) -> Vec<i32> {
-
-    let mut index = 0;
+pub fn bubble_sort(mut vector: Vec<i32>) -> Vec<i32> {
+    let mut outer_index = 0;
     let length = vector.len();
-    
+
+    while outer_index < length {
+        let mut inner_index = outer_index + 1;
+        while inner_index < length {
+            if vector[inner_index] < vector[inner_index - 1] {
+                let tmp = vector[inner_index];
+                vector[inner_index] = vector[inner_index - 1];
+                vector[inner_index - 1] = tmp;
+            }
+            inner_index += 1;
+        }
+        outer_index += 1;
+    }
+
     return vector;
 }
--- tests/bubble_sort.rs
+++ tests/bubble_sort.rs
@@ -6,8 +6,10 @@ fn is_sorted(vector: &Vec<i32>) -> bool {
     for el in vector.iter() {
         match previous {
             Some(x) => {
-                if x > *el {return false}
-            },
+                if x > *el {
+                    return false;
+                }
+            }
             _ => {
                 previous = Some(*el);
             }
@@ -27,20 +29,29 @@ fn it_works() {
 fn it_works_for_one_element() {
     let mut vector: Vec<i32> = vec![1];
     vector = bubble_sort(vector);
-    assert_eq!(is_sorted(&vector), true);
+    assert!(is_sorted(&vector));
 }
 
 #[test]
 fn it_works_for_two_elements_sorted() {
     let mut vector: Vec<i32> = vec![1, 2];
     vector = bubble_sort(vector);
-    assert_eq!(is_sorted(&vector), true);
+    assert!(is_sorted(&vector));
 }
-
 
 #[test]
 fn it_works_for_two_elements_unsorted() {
     let mut vector: Vec<i32> = vec![2, 1];
     vector = bubble_sort(vector);
-    assert_eq!(is_sorted(&vector), true);
+    assert!(is_sorted(&vector));
+}
+
+#[test]
+fn it_works_for_a_bigger_vector() {
+    let mut vector: Vec<i32> = vec![
+        213, 12, 123432, 11224, 6867, 32413, 78787653, 1213, 122, 4446, 146575, 1123245, 654654,
+        1231,
+    ];
+    vector = bubble_sort(vector);
+    assert!(is_sorted(&vector));
 }
